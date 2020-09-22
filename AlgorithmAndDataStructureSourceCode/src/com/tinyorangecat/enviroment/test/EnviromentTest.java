ec456d331c6e437434f436214327146fef1a10e7
TinyOrangeCat <xiroiyuki@hotmail.com>
2020-09-22 16:18:15 +0800

    Init the project source code.

---
+++ AlgorithmAndDataStructureSourceCode/src/com/tinyorangecat/enviroment/test/EnviromentTest.java
@@ -0,0 +1,11 @@
+package com.tinyorangecat.enviroment.test;
+
+import org.junit.Test;
+
+public class EnviromentTest {
+
+    @Test
+    public void TestFirst(){
+        System.out.println("Hello Java!!");
+    }
+}
